9ae5c7c5e93332a56d740d92f77944ec2bf6f67d
SammerLegend <baliguatsammer@gmail.com>
2026-08-22 16:45:46 +0800

    feat: Add clear game and player actions

--- tictactoesamer/Form1.Designer.cs
+++ tictactoesamer/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace tictactoesamer
+﻿namespace TicTacToe
 {
     partial class Form1
     {
@@ -28,163 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_Computerscore = new System.Windows.Forms.Label();
+            this.lbl_Playerscore = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // b1
             // 
-            this.button1.BackColor = System.Drawing.Color.Blue;
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button1.Location = new System.Drawing.Point(171, 211);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 96);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Y";
-            this.button1.UseVisualStyleBackColor = false;
+            this.b1.BackColor = System.Drawing.Color.Blue;
+            this.b1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b1.Location = new System.Drawing.Point(171, 211);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(89, 96);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "Y";
+            this.b1.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // b2
             // 
-            this.button2.BackColor = System.Drawing.Color.Blue;
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button2.Location = new System.Drawing.Point(297, 211);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 96);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Y";
-            this.button2.UseVisualStyleBackColor = false;
+            this.b2.BackColor = System.Drawing.Color.Blue;
+            this.b2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b2.Location = new System.Drawing.Point(297, 211);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(89, 96);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "Y";
+            this.b2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // b3
             // 
-            this.button3.BackColor = System.Drawing.Color.Blue;
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button3.Location = new System.Drawing.Point(422, 211);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(89, 96);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Y";
-            this.button3.UseVisualStyleBackColor = false;
+            this.b3.BackColor = System.Drawing.Color.Blue;
+            this.b3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b3.Location = new System.Drawing.Point(422, 211);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(89, 96);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "Y";
+            this.b3.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // b6
             // 
-            this.button4.BackColor = System.Drawing.Color.Blue;
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button4.Location = new System.Drawing.Point(422, 333);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(89, 96);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Y";
-            this.button4.UseVisualStyleBackColor = false;
+            this.b6.BackColor = System.Drawing.Color.Blue;
+            this.b6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b6.Location = new System.Drawing.Point(422, 333);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(89, 96);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "Y";
+            this.b6.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // b5
             // 
-            this.button5.BackColor = System.Drawing.Color.Blue;
-            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button5.Location = new System.Drawing.Point(297, 333);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(89, 96);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Y";
-            this.button5.UseVisualStyleBackColor = false;
+            this.b5.BackColor = System.Drawing.Color.Blue;
+            this.b5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b5.Location = new System.Drawing.Point(297, 333);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(89, 96);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "Y";
+            this.b5.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // b4
             // 
-            this.button6.BackColor = System.Drawing.Color.Blue;
-            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button6.Location = new System.Drawing.Point(171, 333);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(89, 96);
-            this.button6.TabIndex = 3;
-            this.button6.Text = "Y";
-            this.button6.UseVisualStyleBackColor = false;
+            this.b4.BackColor = System.Drawing.Color.Blue;
+            this.b4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b4.Location = new System.Drawing.Point(171, 333);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(89, 96);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "Y";
+            this.b4.UseVisualStyleBackColor = false;
             // 
-            // button7
+            // b9
             // 
-            this.button7.BackColor = System.Drawing.Color.Blue;
-            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button7.Location = new System.Drawing.Point(422, 451);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(89, 96);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "Y";
-            this.button7.UseVisualStyleBackColor = false;
+            this.b9.BackColor = System.Drawing.Color.Blue;
+            this.b9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b9.Location = new System.Drawing.Point(422, 451);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(89, 96);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "Y";
+            this.b9.UseVisualStyleBackColor = false;
             // 
-            // button8
+            // b8
             // 
-            this.button8.BackColor = System.Drawing.Color.Blue;
-            this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button8.Location = new System.Drawing.Point(297, 451);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(89, 96);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "Y";
-            this.button8.UseVisualStyleBackColor = false;
+            this.b8.BackColor = System.Drawing.Color.Blue;
+            this.b8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b8.Location = new System.Drawing.Point(297, 451);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(89, 96);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "Y";
+            this.b8.UseVisualStyleBackColor = false;
             // 
-            // button9
+            // b7
             // 
-            this.button9.BackColor = System.Drawing.Color.Blue;
-            this.button9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button9.Location = new System.Drawing.Point(171, 451);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(89, 96);
-            this.button9.TabIndex = 6;
-            this.button9.Text = "Y";
-            this.button9.UseVisualStyleBackColor = false;
+            this.b7.BackColor = System.Drawing.Color.Blue;
+            this.b7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.b7.Location = new System.Drawing.Point(171, 451);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(89, 96);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "Y";
+            this.b7.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // lbl_1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(167, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 31);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Player:";
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_1.Location = new System.Drawing.Point(167, 47);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(102, 31);
+            this.lbl_1.TabIndex = 9;
+            this.lbl_1.Text = "PLAYER";
             // 
-            // label2
+            // lbl_2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label2.Location = new System.Drawing.Point(167, 115);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 31);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "CPU:";
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_2.Location = new System.Drawing.Point(167, 115);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(68, 31);
+            this.lbl_2.TabIndex = 10;
+            this.lbl_2.Text = "CPU:";
             // 
-            // label3
+            // lbl_Computerscore
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label3.Location = new System.Drawing.Point(270, 115);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 31);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "0";
+            this.lbl_Computerscore.AutoSize = true;
+            this.lbl_Computerscore.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Computerscore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_Computerscore.Location = new System.Drawing.Point(270, 115);
+            this.lbl_Computerscore.Name = "lbl_Computerscore";
+            this.lbl_Computerscore.Size = new System.Drawing.Size(28, 31);
+            this.lbl_Computerscore.TabIndex = 12;
+            this.lbl_Computerscore.Text = "0";
             // 
-            // label4
+            // lbl_Playerscore
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label4.Location = new System.Drawing.Point(270, 47);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(28, 31);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "0";
+            this.lbl_Playerscore.AutoSize = true;
+            this.lbl_Playerscore.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Playerscore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_Playerscore.Location = new System.Drawing.Point(270, 47);
+            this.lbl_Playerscore.Name = "lbl_Playerscore";
+            this.lbl_Playerscore.Size = new System.Drawing.Size(28, 31);
+            this.lbl_Playerscore.TabIndex = 11;
+            this.lbl_Playerscore.Text = "0";
             // 
             // Form1
             // 
@@ -192,19 +192,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(848, 647);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_Computerscore);
+            this.Controls.Add(this.lbl_Playerscore);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -214,19 +214,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_Computerscore;
+        private System.Windows.Forms.Label lbl_Playerscore;
     }
 }
 
